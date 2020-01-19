71266dfc944a6473425598e0ecf9e5697fece4f3
J. Famous <joshua.famous@gmail.com>
2020-01-18 22:00:51 -0500

    Build probes

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ class Pylon_AI(sc2.BotAI):
 
 	async def on_step(self, iteration):
 		await self.distribute_workers()
+		await self.build_workers()
+
+	async def build_workers(self):
+		for nexus in self.units(NEXUS).ready.noqueue:
+			if self.can_afford(PROBE):
+				await self.do(nexus.train(PROBE))
 
 run_game(maps.get("TritonLE"), [
 	    Bot(Race.Protoss, Pylon_AI()),
