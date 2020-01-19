3cf229b2bc64fb4ed1ed55a614e6caee92f3d8ce
J. Famous <joshua.famous@gmail.com>
2020-01-18 23:18:58 -0500

    Add build priorities, should acutally be a queue

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Pylon_AI(sc2.BotAI):
 	hr_supplyTrigger = 5
 	hr_gatewayMultiplier = 3
 	hr_expansionTime = 240 # Expansion time in seconds
-	hr_buildPriorities = {"PROBE": 1, "NEXUS": 10, "PYLON": 3, "GATEWAY": 2, "ZEALOT": 2} # This should be situational, generalize for now
+	hr_buildPriorities = {"PROBE": 0, "NEXUS": 10, "PYLON": 3, "GATEWAY": 2, "ZEALOT": 1} # This should be situational, generalize for now
 
 	# Local Vars
 	buildPriority = 0
@@ -24,30 +24,41 @@ class Pylon_AI(sc2.BotAI):
 
 	async def build_workers(self):
 		for nexus in self.units(NEXUS).ready.noqueue:
-			if self.can_afford(PROBE):
+			if self.can_afford(PROBE) and self.hr_buildPriorities["PROBE"] >= self.buildPriority:
 				await self.do(nexus.train(PROBE))
 
 	async def build_zealots(self):
 		for gateway in self.units(GATEWAY).ready.noqueue:
-			if self.can_afford(ZEALOT):
+			if self.can_afford(ZEALOT) and self.hr_buildPriorities["ZEALOT"] >= self.buildPriority:
 				await self.do(gateway.train(ZEALOT))
 
 	async def build_pylons(self):
 		if self.supply_left < self.hr_supplyTrigger and not self.already_pending(PYLON):
 			nexuses = self.units(NEXUS).ready
 			if nexuses.exists:
-				if self.can_afford(PYLON):
+				if(self.buildPriority < self.hr_buildPriorities["PYLON"]):
+					self.buildPriority = self.hr_buildPriorities["PYLON"]
+				if self.can_afford(PYLON) and self.hr_buildPriorities["PYLON"] >= self.buildPriority:
 					await self.build(PYLON, near=nexuses.first)
+					self.buildPriority = 0
 
 	async def build_gateways(self):
 		pylons = self.units(PYLON).ready
 		if pylons.exists:
-			if self.can_afford(GATEWAY) and len(self.units(GATEWAY)) < (self.hr_gatewayMultiplier * len(self.units(NEXUS))):
-				await self.build(GATEWAY, near=pylons.first)
+			if len(self.units(GATEWAY)) < (self.hr_gatewayMultiplier * len(self.units(NEXUS))):
+				if(self.buildPriority < self.hr_buildPriorities["GATEWAY"]):
+					self.buildPriority = self.hr_buildPriorities["GATEWAY"]
+				if self.can_afford(GATEWAY) and self.hr_buildPriorities["GATEWAY"] >= self.buildPriority:
+					await self.build(GATEWAY, near=pylons.first)
+					self.buildPriority = 0
 
 	async def expand(self):
-		if self.can_afford(NEXUS) and (self.time / self.hr_expansionTime) > len(self.units(NEXUS)):
-			await self.expand_now()
+		if (self.time / self.hr_expansionTime) > len(self.units(NEXUS)):
+			if(self.buildPriority < self.hr_buildPriorities["NEXUS"]):
+				self.buildPriority = self.hr_buildPriorities["NEXUS"]
+			if self.can_afford(NEXUS) and self.hr_buildPriorities["NEXUS"] >= self.buildPriority:
+				await self.expand_now()
+				self.buildPriority = 0
 
 run_game(maps.get("TritonLE"), [
 	    Bot(Race.Protoss, Pylon_AI()),
