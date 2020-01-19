4b9c465f8690690dbf4d242125785564a8565552
J. Famous <joshua.famous@gmail.com>
2020-01-18 23:36:55 -0500

    Add queue and node

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Pylon_AI(sc2.BotAI):
 	hr_buildPriorities = {"PROBE": 0, "NEXUS": 10, "PYLON": 3, "GATEWAY": 2, "ZEALOT": 1} # This should be situational, generalize for now
 
 	# Local Vars
-	buildPriority = 0
+	buildPlans = Queue()
 
 	async def on_step(self, iteration):
 		await self.distribute_workers()
@@ -39,8 +39,8 @@ class Pylon_AI(sc2.BotAI):
 				if(self.buildPriority < self.hr_buildPriorities["PYLON"]):
 					self.buildPriority = self.hr_buildPriorities["PYLON"]
 				if self.can_afford(PYLON) and self.hr_buildPriorities["PYLON"] >= self.buildPriority:
-					await self.build(PYLON, near=nexuses.first)
 					self.buildPriority = 0
+					await self.build(PYLON, near=nexuses.first)
 
 	async def build_gateways(self):
 		pylons = self.units(PYLON).ready
@@ -49,18 +49,143 @@ class Pylon_AI(sc2.BotAI):
 				if(self.buildPriority < self.hr_buildPriorities["GATEWAY"]):
 					self.buildPriority = self.hr_buildPriorities["GATEWAY"]
 				if self.can_afford(GATEWAY) and self.hr_buildPriorities["GATEWAY"] >= self.buildPriority:
-					await self.build(GATEWAY, near=pylons.first)
 					self.buildPriority = 0
+					await self.build(GATEWAY, near=pylons.first)
 
 	async def expand(self):
 		if (self.time / self.hr_expansionTime) > len(self.units(NEXUS)):
 			if(self.buildPriority < self.hr_buildPriorities["NEXUS"]):
 				self.buildPriority = self.hr_buildPriorities["NEXUS"]
 			if self.can_afford(NEXUS) and self.hr_buildPriorities["NEXUS"] >= self.buildPriority:
-				await self.expand_now()
 				self.buildPriority = 0
+				await self.expand_now()
 
 run_game(maps.get("TritonLE"), [
-	    Bot(Race.Protoss, Pylon_AI()),
-	    Computer(Race.Terran, Difficulty.Easy)
+		Bot(Race.Protoss, Pylon_AI()),
+		Computer(Race.Terran, Difficulty.Easy)
 	], realtime=True)
+
+class Queue():
+
+	self.tail = None
+	self.head = None
+
+class Node:
+
+	def __init__(self, value):
+		self.value = value  
+		self.next = None 
+	
+	def __str__(self):
+		return "Node({})".format(self.value) 
+
+	__repr__ = __str__
+
+class Queue:
+
+	def __init__(self):
+		self.head=None
+		self.tail=None
+		self.count=0
+
+	def __str__(self):
+		temp=self.head
+		out=[]
+		while temp:
+			out.append(str(temp.value))
+			temp=temp.next
+		out=' '.join(out)
+		return f'Head:{self.head}\nTail:{self.tail}\nQueue:{out}'
+
+	__repr__=__str__
+
+	def isEmpty(self):
+
+		# Use length to get whether we are empty
+		if self.count == 0 :
+			return True
+		else :
+			return False
+
+	def enqueue(self, x):
+
+		# Create node
+		newNode = Node(x)
+
+		# Case for first node
+		if self.count == 0:
+			self.head = newNode
+		else:
+			self.tail.next = newNode
+		
+		self.tail = newNode
+
+		# Update count
+		self.count += 1
+
+	def dequeue(self):
+
+		# Check list size, cases for last removal and empty list
+		if self.count == 0 :
+			return None # NOTE the two different doctests provided use 'queue is empty' and none here, not sure which is correct for prompt
+		elif self.count == 1 :
+			returnValue = self.head.value
+			self.head = None
+			self.tail = None
+			self.count = 0
+			return returnValue
+		else:
+			returnValue = self.head.value
+			self.head = self.head.next
+			self.count -= 1
+			return returnValue
+
+	def __len__(self):
+
+		return self.count
+
+	def reverse(self): 
+
+		# We do not need to reverse length 1 or less
+		if self.count > 1 :
+
+			# Save all elements to list
+			nodeList = []
+			thisNode = self.head
+
+			while(thisNode.next):
+				nodeList.append(thisNode)
+				if thisNode.next == self.tail:
+					nodeList.append(self.tail)
+					break
+				thisNode = thisNode.next
+
+			# Iterate and re-build
+			for index in range(self.count - 1, -1, -1):
+				nodeList[index].next = nodeList[index-1]
+
+			# Set new head and tail
+			self.tail = nodeList[0]
+			self.tail.next = None
+			self.head = nodeList[self.count - 1]
+
+	def contains(self, item):
+
+		# Handle empty queue
+		if(self.count == 0):
+			return False
+
+		# Iterate and search for value
+		thisNode = self.head
+		while(thisNode.next):
+			if(thisNode.value == item):
+				return True
+			else:
+				thisNode = thisNode.next
+
+		# Check tail
+		if(self.tail.value == item):
+			return True
+
+		# Except false
+		return False
