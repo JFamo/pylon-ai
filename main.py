d4e7c9b1c7b0c8a90d3a47bd53bed1620fdc25fb
J. Famous <joshua.famous@gmail.com>
2020-01-18 23:04:21 -0500

    Expansions and build priorities

--- main.py
+++ main.py
@@ -8,11 +8,17 @@ class Pylon_AI(sc2.BotAI):
 	# Heuristics
 	hr_supplyTrigger = 5
 	hr_gatewayMultiplier = 3
+	hr_expansionTime = 240 # Expansion time in seconds
+	hr_buildPriorities = {"PROBE": 1, "NEXUS": 10, "PYLON": 3, "GATEWAY": 2, "ZEALOT": 2} # This should be situational, generalize for now
+
+	# Local Vars
+	buildPriority = 0
 
 	async def on_step(self, iteration):
 		await self.distribute_workers()
 		await self.build_workers()
 		await self.build_pylons()
+		await self.expand()
 		await self.build_gateways()
 		await self.build_zealots()
 
@@ -38,6 +44,10 @@ class Pylon_AI(sc2.BotAI):
 		if pylons.exists:
 			if self.can_afford(GATEWAY) and len(self.units(GATEWAY)) < (self.hr_gatewayMultiplier * len(self.units(NEXUS))):
 				await self.build(GATEWAY, near=pylons.first)
+
+	async def expand(self):
+		if self.can_afford(NEXUS) and (self.time / self.hr_expansionTime) > len(self.units(NEXUS)):
+			await self.expand_now()
 
 run_game(maps.get("TritonLE"), [
 	    Bot(Race.Protoss, Pylon_AI()),
