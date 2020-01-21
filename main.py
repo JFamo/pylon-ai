c59843d2060fe90d39a74971416ec3c5a4742710
J. Famous <joshua.famous@gmail.com>
2020-01-21 05:15:10 -0500

    Still fails, call it a night
    TODO- handle error on upgrade queue, migrate heuristics to local file and implement mutation, add tech units

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from sc2.position import Point2
 from sc2.player import Bot, Computer
 from sc2.constants import *
 from sc2.game_data import AbilityData, GameData
-from sc2.game_state import GameState
 from sc2.ids.unit_typeid import UnitTypeId
 from sc2.unit import Unit
 from sc2.units import Units
